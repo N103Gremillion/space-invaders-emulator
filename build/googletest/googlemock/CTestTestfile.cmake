27a982dd8ba118eb6df9602c9417aa154f8c0933
N103Gremillion <nathan103grem@gmail.com>
2025-03-19 16:11:37 -0500

    ignore

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nathan/Desktop/emulators/Space_Ivaders_Emulator/googletest/googlemock
-# Build directory: /home/nathan/Desktop/emulators/Space_Ivaders_Emulator/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nathan/Desktop/emulators/Space_Ivaders_Emulator/googletest/googletest
-# Build directory: /home/nathan/Desktop/emulators/Space_Ivaders_Emulator/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
